07a41e86aa990523904ced7618feb3466af32bc9
WarmuzWojtek <wwarmuz@gmail.com>
2021-09-01 13:58:14 +0200

    Bux logos

--- client/src/components/AppBar/AppBar.jsx
+++ client/src/components/AppBar/AppBar.jsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react'
 import './appBar.css'
-import logoEit from '../../Assets/Images/logos.svg'
+// import logoEit from '../../Assets/Images/logos.svg'
 import Button from '../Button/Button'
 
 
