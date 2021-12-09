c41a6a6eafd522c7f6913397ac101116bfafe3d5
WarmuzWojtek <wwarmuz@gmail.com>
2021-12-09 10:19:49 +0100

    create custom modal

--- client/src/Pages/DemoPlay.jsx
+++ client/src/Pages/DemoPlay.jsx
@@ -1,20 +1,19 @@
-import { useState, useEffect, useRef } from 'react'
-import { gsap } from 'gsap'
-import { Modal } from 'react-responsive-modal';
-import PlayBackground from '../components/PlayBackground/PlayBackground'
-import AppBar from '../components/AppBar/AppBar'
-import GameplayContainer from '../components/GameplayContainer/GameplayContainer'
-import PlaygroundContainer from '../components/PlaygroundContainer/PlaygroundContainer'
-import RulesContainer from '../components/RulesContainer/RulesContainer'
-import ActionsContainer from '../components/ActionsContainer/ActionsContainer'
-import CounterContainer from '../components/CounterContainer/CounterContainer'
-import LanguageSwitch from '../components/LanguageSwitch/LanguageSwitch'
-import Subtitle from '../components/Subtitle/Subtitle'
-import Button from '../components/Button/Button'
-import { words } from '../Constants/words'
+import { useState, useEffect, useRef } from "react";
+import { gsap } from "gsap";
+// import { Modal } from "react-responsive-modal";
+import PlayBackground from "../components/PlayBackground/PlayBackground";
+import AppBar from "../components/AppBar/AppBar";
+import GameplayContainer from "../components/GameplayContainer/GameplayContainer";
+import PlaygroundContainer from "../components/PlaygroundContainer/PlaygroundContainer";
+import RulesContainer from "../components/RulesContainer/RulesContainer";
+import ActionsContainer from "../components/ActionsContainer/ActionsContainer";
+import CounterContainer from "../components/CounterContainer/CounterContainer";
+import LanguageSwitch from "../components/LanguageSwitch/LanguageSwitch";
+import Subtitle from "../components/Subtitle/Subtitle";
+import Button from "../components/Button/Button";
+import { words } from "../Constants/words";
+import Modal from "../components/Modal/Modal";
 // import Playground from '../Components/Playground/Playground'
-
-
 
 import { Transition } from "react-transition-group";
 import { TweenMax } from "gsap/all";
@@ -22,11 +21,10 @@ import { TweenMax } from "gsap/all";
 const startState = { autoAlpha: 0, y: -50 };
 
 const DemoPlay = (props) => {
-
-  const [answer, setAnswer] = useState('');
+  const [answer, setAnswer] = useState("");
   const [questions, setQuestions] = useState(words);
-  const [currentQ, setCurrentQ] = useState('');
-  const [comment, setComment] = useState('');
+  const [currentQ, setCurrentQ] = useState("");
+  const [comment, setComment] = useState("");
   const [currentIndex, setCurrentIndex] = useState();
   const [isEnglishFirst, setIsEnglishFirst] = useState(true);
   const [open, setOpen] = useState(false);
@@ -36,81 +34,89 @@ const DemoPlay = (props) => {
 
   const rightAnswersRef = useRef(null);
   const wrongAnswersRef = useRef(null);
+  const closeModalBtnRef = useRef(null);
+  const modalRef = useRef(null);
   const commentRef = useRef(null);
   const startBtnRef = useRef(null);
   const learnBtnRef = useRef(null);
   const questionRef = useRef(null);
 
   useEffect(() => {
-
-    gsap.fromTo(questionRef.current, {
-      transform: 'scale(0)',
-    }, {
-      transform: 'scale(1.3)',
-      // repeat: 0,
-      duration: 1,
-      // yoyo: "true",
-      // delay: 0,
-      ease: 'bounce',
-    });
-
-
-
+    gsap.fromTo(
+      questionRef.current,
+      {
+        transform: "scale(0)",
+      },
+      {
+        transform: "scale(1.3)",
+        // repeat: 0,
+        duration: 1,
+        // yoyo: "true",
+        // delay: 0,
+        ease: "bounce",
+      }
+    );
   }, [currentQ]);
 
-
   useEffect(() => {
+    gsap.fromTo(
+      commentRef.current,
+      {
+        opacity: 1,
+        letterSpacing: "1px",
+        transform: "scale(1)",
+      },
+      {
+        opacity: 0,
+        letterSpacing: "5px",
+        transform: "scale(2.5)",
+        // repeat: 0,
+        duration: 1,
+        yoyo: "true",
+        // delay: 0,
+        ease: "power3",
+      }
+    );
 
-    gsap.fromTo(commentRef.current, {
-      opacity: 1,
-      letterSpacing: '1px',
-      transform: 'scale(1)',
-    }, {
-      opacity: 0,
-      letterSpacing: '5px',
-      transform: 'scale(2.5)',
-      // repeat: 0,
-      duration: 1,
-      yoyo: "true",
-      // delay: 0,
-      ease: 'power3',
-    });
-
-    if (questions.length < 1) { endGame() }
-
+    if (questions.length < 1) {
+      endGame();
+    }
   }, [rightAnswers, wrongAnswers]);
 
   useEffect(() => {
-
-    gsap.fromTo(rightAnswersRef.current, {
-      transform: "translateY(-40px)"
-    }, {
-      transform: "translateY(0)",
-      repeat: 0,
-      duration: 1,
-      delay: 0,
-      ease: 'bounce',
-    });
-
+    gsap.fromTo(
+      rightAnswersRef.current,
+      {
+        transform: "translateY(-40px)",
+      },
+      {
+        transform: "translateY(0)",
+        repeat: 0,
+        duration: 1,
+        delay: 0,
+        ease: "bounce",
+      }
+    );
   }, [rightAnswers]);
 
   useEffect(() => {
-
-    gsap.fromTo(wrongAnswersRef.current, {
-      transform: "translateY(-40px)"
-    }, {
-      transform: "translateY(0)",
-      repeat: 0,
-      duration: 1,
-      delay: 0,
-      ease: 'bounce',
-    });
-
+    gsap.fromTo(
+      wrongAnswersRef.current,
+      {
+        transform: "translateY(-40px)",
+      },
+      {
+        transform: "translateY(0)",
+        repeat: 0,
+        duration: 1,
+        delay: 0,
+        ease: "bounce",
+      }
+    );
   }, [wrongAnswers]);
 
-
   function handleAnswerChange(e) {
-    setAnswer(e.target.value)
+    setAnswer(e.target.value);
   }
   function handleStartBtn() {
     const index = Math.floor(Math.random() * questions.length);
@@ -121,32 +127,37 @@ const DemoPlay = (props) => {
   }
 
   function handleGoodAnswer() {
-    setComment('very good');
-    setRightAnswers(rightAnswers => rightAnswers + 1);
+    setComment("very good");
+    setRightAnswers((rightAnswers) => rightAnswers + 1);
     handleNextQuestion();
-
   }
   function handleWrongAnswer() {
-    setComment('you`re wrong');
-    setWrongAnswers(wrongAnswers => wrongAnswers + 1);
+    setComment("you`re wrong");
+    setWrongAnswers((wrongAnswers) => wrongAnswers + 1);
     handleNextQuestion();
   }
 
   function handleAnswerSubmit(e) {
     e.preventDefault();
-    isEnglishFirst ?
-      questions[currentIndex].a === answer ? handleGoodAnswer() : handleWrongAnswer() :
-      questions[currentIndex].q === answer ? handleGoodAnswer() : handleWrongAnswer()
+    isEnglishFirst
+      ? questions[currentIndex].a === answer
+        ? handleGoodAnswer()
+        : handleWrongAnswer()
+      : questions[currentIndex].q === answer
+      ? handleGoodAnswer()
+      : handleWrongAnswer();
   }
-
 
   function endGame() {
-    setAnswer('');
-    alert(`Twój wynik to ${(rightAnswers / (rightAnswers + wrongAnswers) * 100).toFixed(0)} %`);
-    window.location = '/';
+    setAnswer("");
+    alert(
+      `Twój wynik to ${(
+        (rightAnswers / (rightAnswers + wrongAnswers)) *
+        100
+      ).toFixed(0)} %`
+    );
+    window.location = "/";
   }
-
-
 
   function handleNextQuestion() {
     const first = questions.slice(0, currentIndex);
@@ -158,7 +169,7 @@ const DemoPlay = (props) => {
       const currentQuestion = newQuestions[index];
       setCurrentIndex(index);
       setCurrentQ(isEnglishFirst ? currentQuestion.q : currentQuestion.a);
-      setAnswer('');
+      setAnswer("");
     }
   }
 
@@ -167,82 +178,104 @@ const DemoPlay = (props) => {
   }
 
   function handleLearnBtn() {
-    setOpen(true)
+    modalRef.current.style.transform = "translateY(0)";
   }
 
+  function handleCloseModal() {
+    modalRef.current.style.transform = "translateY(-100%)";
+  }
 
   return (
-
     <Transition
       unmountOnExit
       in={props.show}
       timeout={1000}
-      onEnter={node => TweenMax.set(node, startState)}
+      onEnter={(node) => TweenMax.set(node, startState)}
       addEndListener={(node, done) => {
         TweenMax.to(node, 0.5, {
           autoAlpha: props.show ? 1 : 0,
           y: props.show ? 0 : 50,
-          onComplete: done
+          onComplete: done,
         });
       }}
     >
-
-
       <PlayBackground>
         <AppBar />
         <PlaygroundContainer>
           <GameplayContainer>
             <CounterContainer>
-              <Subtitle content="Good answers : " className='answers' />
-              <Subtitle id={rightAnswersRef} content={rightAnswers} className='answers' />
-              <Subtitle content="Wrong answers : " className='answers' />
-              <Subtitle id={wrongAnswersRef} content={wrongAnswers} className='answers' />
+              <Subtitle content="Good answers : " className="answers" />
+              <Subtitle
+                id={rightAnswersRef}
+                content={rightAnswers}
+                className="answers"
+              />
+              <Subtitle content="Wrong answers : " className="answers" />
+              <Subtitle
+                id={wrongAnswersRef}
+                content={wrongAnswers}
+                className="answers"
+              />
             </CounterContainer>
-            <Subtitle content="Gamemode : demoplay" className='gamemode' />
-            <Subtitle content="Enter a translated word" className='rules' />
-            <Subtitle id={questionRef} className='task' content={currentQ} />
+            <Subtitle content="Gamemode : demoplay" className="gamemode" />
+            <Subtitle content="Enter a translated word" className="rules" />
+            <Subtitle id={questionRef} className="task" content={currentQ} />
             <form onSubmit={handleAnswerSubmit}>
               <input
-                className='input'
-                type='text'
-                placeholder='your answer'
+                className="input"
+                type="text"
+                placeholder="your answer"
                 onChange={handleAnswerChange}
                 value={answer}
               />
             </form>
-            <Subtitle id={commentRef} className='comment' content={comment} />
+            <Subtitle id={commentRef} className="comment" content={comment} />
           </GameplayContainer>
           <ActionsContainer>
             <Button
               id={learnBtnRef}
               onClick={handleLearnBtn}
-              className='learnBtn'
-              content='Learn' />
-            {/* <Modal style={{ background: 'black' }} open={open} onClose={() => setOpen(false)}>
-              <h2 style={{ textAlign: 'center' }}>Demoplay Module</h2>
-              {words.map(word => (
-                <div key={word.a} className='modalContainer'>
-                  <p className='modalWord' style={{ color: 'blue' }}>{word.q}</p>
-                  <p className='modalWord'>:</p>
-                  <p className='modalWord'>{word.a}</p>
+              className="learnBtn"
+              content="Learn"
+            />
+            <Modal ref={modalRef}>
+              <h3 className="modalTitle">Demoplay Module</h3>
+              {words.map((word) => (
+                <div key={word.a} className="modalContainer">
+                  <p className="modalWord">{word.q}</p>
+                  <p className="modalWord">:</p>
+                  <p className="modalWord">{word.a}</p>
                 </div>
               ))}
-            </Modal> */}
+              <Button
+                id={closeModalBtnRef}
+                className="redBtn, redBtn-auto"
+                onClick={handleCloseModal}
+                content="Close"
+              />
+            </Modal>
             <Button
               id={startBtnRef}
               onClick={handleStartBtn}
-              className='learnBtn'
-              content='Start test' />
+              className="learnBtn"
+              content="Start test"
+            />
             <RulesContainer>
-              <Subtitle content="Rules" className='rules' />
-              <Subtitle content='Here are some words to translate. Choose from which language you want to translate, and hit "start" button. Enter a translated word into "your answer" field and hit enter. At the end you will see your score.' className='rulesDescription' />
-              <LanguageSwitch isEnglishFirst={isEnglishFirst} onClickFn={handleLanguageSwitch} />
+              <Subtitle content="Rules" className="rules" />
+              <Subtitle
+                content='Here are some words to translate. Choose from which language you want to translate, and hit "start" button. Enter a translated word into "your answer" field and hit enter. At the end you will see your score.'
+                className="rulesDescription"
+              />
+              <LanguageSwitch
+                isEnglishFirst={isEnglishFirst}
+                onClickFn={handleLanguageSwitch}
+              />
             </RulesContainer>
           </ActionsContainer>
         </PlaygroundContainer>
       </PlayBackground>
     </Transition>
   );
-}
+};
 
 export default DemoPlay;
